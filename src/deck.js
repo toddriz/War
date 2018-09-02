aab3f40171d831598205a30949e89b42136acc7d
Todd Risenmay <toddriz@gmail.com>
2018-09-01 22:29:21 -0600

    more small formatting changes

--- src/deck.js
+++ src/deck.js
@@ -1,4 +1,5 @@
 const _ = require('lodash');
+
 const suits = ['Spades', 'Clubs', 'Hearts', 'Diamonds'];
 const cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
@@ -15,8 +16,7 @@ exports.getDeck = () => {
 exports.getCardStrength = card => {
     if (!card) {
         return -1;
-    }
-    if (card.slice(0, 2) === '10') {
+    } else if (card.slice(0, 2) === '10') {
         return cards.indexOf('10');
     } else {
         return cards.indexOf(card.slice(0, 1));
